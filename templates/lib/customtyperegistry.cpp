7a8ace5dcc265a0c5c5cdb097a9a8e1074fa03f1
Stephen Kelly <steveire@gmail.com>
2013-09-11 14:19:39 +0200

    Remove specialized container access code from Grantlee.
    The logic for accessing containers, smart pointers to QObject derived
    types and QObject subclasses is now part of Qt.
    
    The TypeAccessors remain for custom 3rd party types.

--- templates/lib/customtyperegistry.cpp
+++ templates/lib/customtyperegistry.cpp
@@ -31,14 +31,8 @@
 
 using namespace Grantlee;
 
-namespace Grantlee
+static QVariantList mevToList(const MetaEnumVariable mev)
 {
-
-template<>
-QVariantList VariantToList<MetaEnumVariable>::doConvert( const QVariant &obj )
-{
-  const MetaEnumVariable mev = obj.value<MetaEnumVariable>();
-
   if ( mev.value != -1 )
     return QVariantList();
 
@@ -49,41 +43,19 @@ QVariantList VariantToList<MetaEnumVariable>::doConvert( const QVariant &obj )
   return list;
 }
 
-template<>
-int CustomTypeRegistry::registerBuiltInMetatype<QObject*>()
-{
-  QVariant ( *lf )( const QVariant&, const QString& ) = LookupTrait<QObject*, QObject*>::doLookUp;
-
-  const int id = qMetaTypeId<QObject*>();
-
-  registerLookupOperator( id, reinterpret_cast<MetaType::LookupFunction>( lf ) );
-
-  return id;
-}
-
-}
-
 CustomTypeRegistry::CustomTypeRegistry()
 {
-  registerBuiltInMetatype<QObject*>();
-
   // Grantlee Types
   registerBuiltInMetatype<SafeString>();
   registerBuiltInMetatype<MetaEnumVariable>();
 
-  registerVariantToList<MetaEnumVariable>();
+  QMetaType::registerConverter<MetaEnumVariable, QVariantList>(&mevToList);
 }
 
 void CustomTypeRegistry::registerLookupOperator( int id, MetaType::LookupFunction f )
 {
   CustomTypeInfo &info = types[id];
   info.lookupFunction = f;
-}
-
-void CustomTypeRegistry::registerToListOperator( int id, MetaType::ToVariantListFunction f )
-{
-  CustomTypeInfo &info = types[id];
-  info.toVariantListFunction = f;
 }
 
 QVariant CustomTypeRegistry::lookup( const QVariant &object, const QString &property ) const
@@ -114,41 +86,7 @@ QVariant CustomTypeRegistry::lookup( const QVariant &object, const QString &prop
   return lf( object, property );
 }
 
-QVariantList CustomTypeRegistry::toVariantList( const QVariant &variant ) const
-{
-  if ( !variant.isValid() )
-    return QVariantList();
-
-  const int id = variant.userType();
-  MetaType::ToVariantListFunction tvlf;
-
-  {
-    if ( !types.contains( id ) ) {
-      qWarning() << "Don't know how to handle metatype for list" << QMetaType::typeName( id );
-      // :TODO: Print out error message
-      return QVariantList();
-    }
-
-    const CustomTypeInfo &info = types[id];
-    if ( !info.toVariantListFunction ) {
-      qWarning() << "No toList function for metatype" << QMetaType::typeName( id );
-      tvlf = 0;
-      // :TODO: Print out error message
-      return QVariantList();
-    }
-
-    tvlf = info.toVariantListFunction;
-  }
-
-  return tvlf( variant );
-}
-
 bool CustomTypeRegistry::lookupAlreadyRegistered( int id ) const
 {
   return types.contains( id ) && types.value( id ).lookupFunction != 0;
-}
-
-bool CustomTypeRegistry::toListAlreadyRegistered( int id ) const
-{
-  return types.value( id ).toVariantListFunction != 0;
 }
